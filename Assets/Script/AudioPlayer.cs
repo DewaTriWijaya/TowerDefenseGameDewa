624c398b90ce2c6ede9bc579f160dc39982bc0da
Dewa <73357308+DewaTriWijaya@users.noreply.github.com>
2021-10-29 13:50:31 +0700

    Add project to github

---
+++ Assets/Script/AudioPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+    private static AudioPlayer _Instance = null;
+    public static AudioPlayer Instance
+    {
+        get
+        {
+            if(_Instance == null)
+            {
+                _Instance = FindObjectOfType<AudioPlayer>();
+            }
+            return _Instance;
+        }
+    }
+
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private List<AudioClip> _audioClips;
+
+    public void PlaySFX(string name)
+    {
+        AudioClip sfx = _audioClips.Find(s => s.name == name);
+        if(sfx == null)
+        {
+            return;
+        }
+        _audioSource.PlayOneShot(sfx);
+    }
+}
